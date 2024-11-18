bf8a4f96293bfead1ab12c5dca9f5043ab33fec9
chenliying <chenliying@carben.me>
2024-11-18 12:51:24 +0800

    去掉短信function

--- app/lib/AliyunUtils.php
+++ app/lib/AliyunUtils.php
@@ -11,37 +11,5 @@ class AliyunUtils
     /**
      * 阿里云短信发送新版
      */
-    function sendVerifyCode($phone, $code)
-    {
-        $template="SMS_468970383";
-        $signName = 'MIMO梦墨科技';
-        $option = ['sms_key' => 'LTAI5tLt5tGeTWezRXSwQJwH', 'sms_secret' => 'QW45RvmcISjBSfWgyiByTXbhYZE1dS'];
-        $config = new Config([
-            "accessKeyId" => $option['sms_key'],
-            "accessKeySecret" => $option['sms_secret'],
-        ]);
-        $config->endpoint = "dysmsapi.aliyuncs.com";
-        $client = new Dysmsapi($config);
-        $sendSmsRequest = new SendSmsRequest(
-            [
-                "signName" => $signName,
-                "templateCode" => $template,
-                "phoneNumbers" => $phone,
-                "templateParam" => json_encode(['code' => $code])
-            ]
-        );
-
-        $runtime = new RuntimeOptions([]);
-        try {
-            $result = $client->sendSmsWithOptions($sendSmsRequest, $runtime);
-            if ($result->body->code != 'OK') {
-                return false;
-            }
-            return true;
-        }
-        catch (\Exception $e) {
-            return false;
-        }
-    }
 
 }
