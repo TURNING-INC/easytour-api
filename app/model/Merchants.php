57435fe14b68f54a4e69e0a5d66c4726e8bf7136
chenliying <chenliying@carben.me>
2024-11-27 12:46:48 +0800

    漏了sql

--- app/model/Merchants.php
+++ app/model/Merchants.php
@@ -8,7 +8,8 @@ class Merchants extends Model
 {
     function getByEncodeId($encodeId, $languageKey='zh_cn', $includeAppSet=false) {
         $field = "id, encode_id, `{$languageKey}_name` as name, logo, banner, `{$languageKey}_address` as address, 
-                    phone, lat, lng, `{$languageKey}_business_hours`, `{$languageKey}_intro` as intro, currency, login_bg";
+                    phone, lat, lng, `{$languageKey}_business_hours` as business_hours, `{$languageKey}_intro` as intro, 
+                    currency, login_bg";
 
         if ($includeAppSet) {
             $field .= " ,wx_app_id, wx_app_secret, wx_mch_id, wx_mch_key";
